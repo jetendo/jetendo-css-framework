085c62276a402fe7eb85fa25a594dcc45a408a85
Bruce Kirkpatrick <bruce@farbeyondcode.com>
2016-05-25 00:06:00 -0400

    updates

--- custom.js
+++ custom.js
@@ -1,5 +1,9 @@
 
 $(document).ready(function() {
+	// this function runs the core framework
+	zJetendoLoaded();
+	
+	// example of a cycle2 slideshow being initiated
 	$("#slideshowHomeDiv1").cycle({
 		timeout:4000
 	});
